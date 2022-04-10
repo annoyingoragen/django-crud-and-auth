097e52135df3d07e97c820c37cc4cba285d628fe
annoyingorangen <fatimaaliforwork@gmail.com>
2022-04-10 14:42:58 +0500

    implements home module

---
+++ home/views.py
@@ -0,0 +1,22 @@
+from re import template
+from django.shortcuts import render
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+# Create your views here.
+
+class HomeView (TemplateView):
+    template_name= 'home/welcome.html'
+    extra_content= {'today':datetime.today()}
+
+# def home(request):
+#     return render (request,'home/welcome.html',{'today':datetime.today()})
+
+def AuthorizedView (LoginRequiredMixin,TemplateView):
+    template_name='home/authorized.html'
+    login_url='/admin'
+
+# @login_required
+# def authorized(request):
+#     return render (request,'home/authorized.html',{'today':datetime.today()})
