d4dd45db7034624a1c974a9d32f6af80c80ad28a
annoyingorangen <fatimaaliforwork@gmail.com>
2022-04-10 22:39:41 +0500

    implements a change in noteslistview
    NotesListView will only display the notes of logged in user

--- notes/urls.py
+++ notes/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('noteslist/',views.NotesListView.as_view(), name='notes.list'),
-    path('notes/<int:pk>',views.NoteDetailView.as_view(), name='notes.detail'),
-    path('notes/<int:pk>/edit',views.NotesUpdateView.as_view(), name='notes.update'),
-    path('notes/<int:pk>/delete',views.NotesDeleteView.as_view(), name='notes.delete'),
-    path('notes/new',views.NotesCreateView.as_view(),name='notes.new'),
+    path('<int:pk>',views.NoteDetailView.as_view(), name='notes.detail'),
+    path('<int:pk>/edit',views.NotesUpdateView.as_view(), name='notes.update'),
+    path('<int:pk>/delete',views.NotesDeleteView.as_view(), name='notes.delete'),
+    path('new',views.NotesCreateView.as_view(),name='notes.new'),
     ]
