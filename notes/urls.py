45cc8be51c8182871c140aee18fd4d244032227f
annoyingorangen <fatimaaliforwork@gmail.com>
2022-04-10 15:39:12 +0500

    add some styling to noteslist and also creates a link between notes and detail

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('noteslist',views.NotesListView.as_view(), name='notes.list'),
-    path('notes/<int:pk>',views.NoteDetailView.as_view())
+    path('notes/<int:pk>',views.NoteDetailView.as_view(), name='notes.detail')
     ]
