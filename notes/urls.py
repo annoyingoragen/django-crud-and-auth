72bfb80e06585fb676da881d10c8d72560a4bb51
annoyingorangen <fatimaaliforwork@gmail.com>
2022-04-10 16:54:09 +0500

    implements note create view using django forms and add styling to form errors

--- notes/urls.py
+++ notes/urls.py
@@ -2,6 +2,7 @@ from django import views
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('noteslist',views.NotesListView.as_view(), name='notes.list'),
-    path('notes/<int:pk>',views.NoteDetailView.as_view(), name='notes.detail')
+    path('noteslist/',views.NotesListView.as_view(), name='notes.list'),
+    path('notes/<int:pk>',views.NoteDetailView.as_view(), name='notes.detail'),
+    path('notes/new',views.NotesCreateView.as_view(),name='notes.new'),
     ]
--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,16 @@
+from pyexpat import model
 from urllib.request import Request
 from django.shortcuts import render
-
-import notes
 from .models import Notes
-from django.views.generic import DetailView,ListView
+from .forms import NotesForm
+from django.views.generic import DetailView,ListView,CreateView
 # Create your views here.
+
+class NotesCreateView(CreateView):
+    model=Notes
+    form_class=NotesForm
+    success_url='/notes/noteslist/'
+
 
 class NotesListView(ListView):
     model=Notes
@@ -12,17 +18,14 @@ class NotesListView(ListView):
     template_name='notes/noteslist.html'
 
 
-
 # def noteslist(request):
 #     notes=Notes.objects.all()
 #     return render(request,'notes/noteslist.html',{'notes':notes})
-
 
 class NoteDetailView(DetailView):
     model=Notes
     context_object_name="note"
     template_name='notes/detail.html'
-
 
 # def detail(request,pk):
 #       note=Notes.objects.get(pk=pk)
