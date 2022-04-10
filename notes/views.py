d0657823a71296314ee191a0e966339fcd35e473
annoyingorangen <fatimaaliforwork@gmail.com>
2022-04-10 22:39:41 +0500

    implements a change in noteslistview which will now only di the notf loggedf logged in user

--- notes/views.py
+++ notes/views.py
@@ -1,17 +1,26 @@
 from pyexpat import model
-from re import template
+from re import S, template
 from urllib.request import Request
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Notes
 from .forms import NotesForm
 from django.views.generic import DetailView,ListView,CreateView,UpdateView,DeleteView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class NotesCreateView(CreateView):
     model=Notes
     form_class=NotesForm
     success_url='/notes/noteslist/'
+
+    def form_valid(self,form):
+        self.object=form.save(commit=False)
+        self.object.user=self.request.user
+        self.object.save()
+
+        return HttpResponseRedirect(self.success_url)
+
 
 class NotesUpdateView(UpdateView):
     model=Notes
@@ -24,11 +33,15 @@ class NotesDeleteView(DeleteView):
     template_name='notes/notes_delete.html'
     success_url='/notes/noteslist/'
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin,ListView):
     model=Notes
     context_object_name="notes"
     template_name='notes/noteslist.html'
+    login_url='/admin/'
 
+    #overriding the original method of listview
+    def get_queryset(self) :
+        return self.request.user.notes.all()
 
 # def noteslist(request):
 #     notes=Notes.objects.all()
